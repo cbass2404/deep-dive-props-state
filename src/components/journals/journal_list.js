b5eafdf4ed22161655ef0e061d64618ccadf0298
Cory Bass <cory.bass.07@gmail.com>
2020-10-19 16:39:05 -0500

    updating in case of break

--- src/components/journals/journal_list.js
+++ src/components/journals/journal_list.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import { JournalEntry } from "./journal_entry";
 
 const rawJournalData = [
   { title: "Post One", content: "Post content", status: "draft" },
-  { title: "Post Two", content: "Post content", status: "punlished" },
-  { title: "Post Three", content: "Post content", status: "punlished" },
-  { title: "Post Four", content: "Post content", status: "punlished" },
+  { title: "Post Two", content: "Post content", status: "published" },
+  { title: "Post Three", content: "Post content", status: "published" },
+  { title: "Post Four", content: "Post content", status: "published" },
 ];
 
 class JournalList extends Component {
@@ -13,13 +14,35 @@ class JournalList extends Component {
 
     this.state = {
       journalData: rawJournalData,
-      greeting: 'Hi there',
-      isOpen: true
+      greeting: "Hi there",
+      isOpen: true,
     };
   }
 
+  clearEntries = () => {
+    this.setState({ journalData: [] });
+  };
+
   render() {
-    return <h2>{this.props.heading}</h2>
+    const journalEntries = this.state.journalData.map((journalEntry) => {
+      return (
+        <div key={journalEntry.title}>
+          <JournalEntry
+            title={journalEntry.title}
+            content={journalEntry.content}
+          />
+        </div>
+      );
+    });
+
+    return (
+      <div>
+        <h2>{this.props.heading}</h2>
+        {journalEntries}
+
+        <button onClick={this.clearEntries}>Clear Journal Entries</button>
+      </div>
+    );
   }
 }
 
